bc853146c9c0a192a32b61d1363492eedad4a206
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-11 19:44:04 +0100

    add a way to check the input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
+function checkInput(n){
+    if(isNaN(n)){
+        console.log("fermato al nan")
+        return false;
+    }
+
+    if(n.length != 4){
+        console.log("fermato al length")
+        return false
+    }
+
+    return true
+}
+
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
-    let num = parseInt(document.getElementById("number").value);
-    
+    let num = document.getElementById("number").value;
+    let flag = checkInput(num)  
+    result.innerText = ''
+    if(flag){
+        result.innerText += 'cacio';
+    }else{
+        result.innerText += 'pepe';
+    }
 })
