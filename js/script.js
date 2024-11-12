411d7c0d83dd503f17d97cca056ab86405df38d6
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-12 12:36:25 +0100

    add all comments

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-import { numberCreation, checkInput } from "./function.js";
+import { numberCreation, checkInput, checkSequence, print,  printW, error} from "./function.js"; //importo le funzioni che mi servono
 
-let arr = numberCreation();
+
+let arr = numberCreation(); //creo l'array di numeri
 console.log(arr)
-form.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    let sequence ='';
-    
-    let num = document.getElementById("number").value; 
-    result.innerText = '';
-    if(checkInput(num)){
 
+form.addEventListener("submit", (event)=>{
+    event.preventDefault(); 
+
+    let num = document.getElementById("number").value; //prendo il valore di input
+
+    if(checkInput(num)){ //se l'input è corretto 
+        let sequence = checkSequence(num, arr); //controllo la sequenza
+        print(num,sequence) // stampi il risultato del controllo
+        if (sequence=='XXXX') { //se hai fatto 4 x
+            printW() //stampi il messaggio di vittoria
+        }
     }else{
-        result.innerText += 'pepe';
+        error(); // in caso non sia stato insreito un valore valido
     }
 })
